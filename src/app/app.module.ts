d983680d5906e66978df89cfe8d418ce9c3d6619
afzsilva <afzsilva@gmail.com>
2023-09-27 10:43:50 -0300

    projeto criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    MenuBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
